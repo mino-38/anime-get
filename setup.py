a5933bfc1900c54d6a5e887f7169414f68b45676
minomushi <wngx5451@yahoo.co.jp>
2021-10-17 08:45:55 -0400

    setup.cfgを追加

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="anime-get",
-    author="minomushi",
-    version="1.0.0",
-    description="Get anime information",
-    keywords="anime",
-    install_requires=["requests"],
-    entry_points={
-        "console_scripts": [
-            "anime-get=main.anime_get:main"
-        ]
-    },
-    url="https://github.com/mino-38/anime-get"
-)
+setup()
